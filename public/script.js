258abd68a54e49b0b5bcc4cb5dad60394830d793
Ronit Baranwal <ronitkumar151002@gmail.com>
2023-08-08 01:37:11 +0530

    yes another commit done today

--- public/script.js
+++ public/script.js
@@ -1,8 +1,38 @@
 const contact = document.getElementById('contacttxt');
 const firstName = document.getElementById('firstnametxt');
 const secondName = document.getElementById('secondnametxt');
-const gmail = document.getElementById('gmail.txt');
+const gmail = document.getElementById('gmailtxt');
+
 const occupation = document.getElementById('occupationtxt');
+const submitBtn = document.getElementById('submit')
+console.log(submitBtn);
+const baseUrl = 'http://127.0.0.1:7000/info';
+const postInfo = async (e) => {
+  e.preventDefault();
+  const data = {
+    "ContactNumber": document.getElementById('contacttxt').value,
+    "FirstName": document.getElementById('firstnametxt').value,
+    'SecondName': document.getElementById('secondnametxt').value,
+    'Occupation': document.getElementById('occupationtxt').value,
+    'Gmail': document.getElementById('gmailtxt').value,
+  };
+  document.getElementById('contacttxt').value = '';
+  document.getElementById('firstnametxt').value = '';
+  document.getElementById('secondnametxt').value = '';
+  document.getElementById('gmailtxt').value = '';
+  document.getElementById('occupationtxt').value = '';
+  const data1 = await fetch(baseUrl , {
+      method: "POST",
+      headers: {
+          "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+  });
+  
+};
+
+submitBtn.addEventListener('click', postInfo);
+submitBtn.addEventListener('click', () => alert('data added success'));
 
 
 
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./public'));
-
-app.get('/data', () => {
-  
+const { connectToDb, getDb } = require('./db');
+let db;
+//db connection
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => console.log("listening to port"));
+    db = getDb();
+  }
 })
-
-
-
-
+app.use(express.static('./public'));
+app.use(express.json());
+app.post('/info', (req, res) => {
+  const object = req.body;
+  console.log(object);
+  db.collection('Contact')
+    .insertOne(object)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ err: 'error' });
+    })
+})
 app.listen(7000, () => console.log('server started at port 7000'));
 
 
